b2766f2a42f51814e1837ccfc5aefc475b35196f
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2018-05-24 17:07:55 +0300

    separation of concerns
    trying view model, live data and fragments with kotlin

---
+++ app/src/main/java/fi/auroralert/model/GeophysicalObservatory.kt
@@ -0,0 +1,48 @@
+package fi.auroralert.model
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.MutableLiveData
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import org.jsoup.Jsoup
+
+data class GeophysicalObservatory(
+        val name: String,
+        val latitude: Double,
+        val longitude: Double) {
+
+    override fun toString(): String = "Name: $name, Lat: $latitude, Lon: $longitude"
+
+}
+
+class GeophysicalObservatoryModel(application: Application): AndroidViewModel(application) {
+
+    private val geoLocations: MutableLiveData<List<GeophysicalObservatory>> = MutableLiveData()
+
+    fun setGeoLocations() {
+        doAsync {
+            val bg = parseGeophysicsObservatories()
+            uiThread { geoLocations.value = bg }
+        }
+    }
+
+    fun getGeoLocations() = geoLocations
+}
+
+/*fun getGeophysicalObservatoryLocations: List<GeophysicalObservatory> {
+    //TODO: parse 1st time (or if new stations), otherwise get from room DB
+}*/
+
+fun parseGeophysicsObservatories(): List<GeophysicalObservatory> {
+    var count = 0
+    val lst = mutableListOf<GeophysicalObservatory>()
+    Jsoup.connect("http://space.fmi.fi/image/realtime/FIN/").get()
+            .select("div#station_map table tr").forEach {
+        if(count++ > 1) lst.add(GeophysicalObservatory(
+                it.select("td")[0].text(),
+                it.select("td")[1].text().toDouble(),
+                it.select("td")[2].text().toDouble()))
+    }
+    return lst
+}
---
+++ app/src/main/java/fi/auroralert/view/GeoObsLocFragment.kt
@@ -0,0 +1,31 @@
+package fi.auroralert.view
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import fi.auroralert.R
+import fi.auroralert.model.GeophysicalObservatoryModel
+
+
+class GeoObsLocFragment: Fragment() {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val gom = ViewModelProviders.of(this).get(GeophysicalObservatoryModel::class.java)
+        gom.setGeoLocations()
+        gom.getGeoLocations().observe(this, Observer {
+            val txtGeo = view!!.findViewById<TextView>(R.id.txt_geo_obs)
+            txtGeo.text = it.toString()
+        })
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.frag_geophysical_observatory_location, container, false)
+    }
+
+}
