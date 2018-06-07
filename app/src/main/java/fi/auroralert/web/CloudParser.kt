b4c3b86b1036be78c2449b7dd6728cb9d4b683b4
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2018-06-07 17:00:10 +0300

    parse XML with Jsoup
    was fun to try retrofit; but I failed to parse XML + SimpleXML parser is deprecated

--- app/src/main/java/fi/auroralert/web/CloudParser.kt
+++ app/src/main/java/fi/auroralert/web/CloudParser.kt
@@ -1,17 +1,39 @@
 package fi.auroralert.web
 
+import android.util.Log
 import fi.auroralert.BuildConfig
-import fi.auroralert.model.CloudXML
-import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
+import fi.auroralert.model.Cloud
+import fi.auroralert.view.TAG
+import org.jsoup.Jsoup
+import org.jsoup.parser.Parser
+import java.text.SimpleDateFormat
+import java.util.*
 
-interface FmiDataService {
+fun parseCloudCover(latlon: String): List<Cloud> {
 
-    @GET("/fmi-apikey/" + BuildConfig.FMI_API_KEY + "/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::multipointcoverage&parameters=totalcloudcover")
-    fun cloudCover(
-            @Query("latlon") latlon: String,
-            @Query("starttime") start: String,
-            @Query("endtime") end: String): Call<CloudXML>
+    val lst = mutableListOf<Cloud>()
+    val df = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    df.setTimeZone(TimeZone.getTimeZone("UTC"))
+    val now = (3600 * 1000) * (System.currentTimeMillis() / (3600 * 1000))
+    Log.d(TAG, "parse cloud: " + df.format(now))
 
+    // &latlon=60.16952,24.93545&starttime=2018-06-06T14:00:00%2B03:00&endtime=2018-06-07T13:00:00%2B03:00
+    val url = "http://data.fmi.fi/fmi-apikey/" + BuildConfig.FMI_API_KEY +
+            "/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point:" +
+            ":multipointcoverage&parameters=totalcloudcover&latlon=" + latlon +
+            "&starttime=" + df.format(now) + "&endtime=" +
+            df.format(Date( now + 6 * 3600 * 1000))
+    val xml = Jsoup.connect(url).parser(Parser.xmlParser()).get()
+    //val test = xml.selectFirst("gml:name") ?: "nope"
+    val loc = xml.getElementsByTag("gml:name").first().text()
+    Log.d(TAG, "parse cloud: " + loc)
+    val reg = xml.getElementsByTag("target:region").first().text()
+    Log.d(TAG, "parse cloud: " + reg)
+    var count = 0
+    xml.getElementsByTag("gml:doubleOrNilReasonTupleList").first().text().split(" ").forEach {
+        Log.d(TAG, "parse cloud: val " + count + ": " + it)
+        lst.add(Cloud(loc, reg, Date( now + (count++) * 3600 * 1000), it.toFloat()))
+    }
+
+    return lst
 }
--- app/src/main/java/fi/auroralert/worker/CloudWorker.kt
+++ app/src/main/java/fi/auroralert/worker/CloudWorker.kt
@@ -4,63 +4,26 @@ import android.preference.PreferenceManager
 import android.util.Log
 import androidx.work.Worker
 import fi.auroralert.R
-import fi.auroralert.model.CloudXML
 import fi.auroralert.view.TAG
-import fi.auroralert.web.FmiDataService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-// TODO: have a look at https://github.com/Tickaroo/tikxml ?
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory
-import java.text.SimpleDateFormat
-import java.util.*
+import fi.auroralert.web.parseCloudCover
 
 class CloudWorker: Worker() {
 
-    var wres: WorkerResult? = null
-
     override fun doWork(): WorkerResult {
-        Log.d(TAG, "parse cloud")
-
-        val retrofit = Retrofit.Builder()
-                .baseUrl("http://data.fmi.fi")
-                .addConverterFactory(SimpleXmlConverterFactory.create())
-                .build()
-        val api = retrofit.create<FmiDataService>(FmiDataService::class.java)
+        Log.d(TAG, "work cloud")
 
         val sp = PreferenceManager.getDefaultSharedPreferences(applicationContext)
         Log.d(TAG, "test cloud: " + sp.getString("pref_cloud", "sharedPref FAILED?"))
-        val df = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
-        df.setTimeZone(TimeZone.getTimeZone("UTC"))
-        //Log.d(TAG, "test cloud 3: " + df.format(Date()))
 
-        // &latlon=60.16952,24.93545&starttime=2018-06-06T14:00:00%2B03:00&endtime=2018-06-07T13:00:00%2B03:00
-        val call = api.cloudCover(
-                sp.getString(
-                        "pref_loc_lat",
-                        applicationContext.resources.getString(R.string.pref_loc_lat_def))
-                        + "," + sp.getString(
-                        "pref_loc_lon",
-                        applicationContext.resources.getString(R.string.pref_loc_lat_def)),
-                df.format(Date()),
-                df.format(Date(System.currentTimeMillis() + 6 * 3600 * 1000)))
-        Log.d(TAG, "try retrofit: " + call.request().url())
+        val lst = parseCloudCover(sp.getString(
+                "pref_loc_lat",
+                applicationContext.resources.getString(R.string.pref_loc_lat_def))
+                + "," + sp.getString(
+                "pref_loc_lon",
+                applicationContext.resources.getString(R.string.pref_loc_lat_def)))
 
-        call.enqueue(object: Callback<CloudXML> {
-            override fun onFailure(call: Call<CloudXML>?, t: Throwable?) {
-                Log.d(TAG, "retrofit failed: " + t?.message)
-                wres = WorkerResult.FAILURE
-            }
-
-            override fun onResponse(call: Call<CloudXML>?, response: Response<CloudXML>?) {
-                Log.d(TAG, "retrofit response?: " + response?.body())
-                wres = WorkerResult.SUCCESS
-            }
-
-        })
-
-        return wres ?: WorkerResult.FAILURE
+        Log.d(TAG, "work cloud: " + lst)
+        return if(lst.isNotEmpty()) WorkerResult.SUCCESS else WorkerResult.FAILURE
     }
 
 }
